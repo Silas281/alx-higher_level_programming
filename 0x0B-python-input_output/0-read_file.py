d0a93a77fcac17a46b0b21da5d336ca3ddf7b8ae
Silas281 <sangminsilas@gmail.com>
2023-02-09 00:24:49 +0000

    Add a function to read text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """Write a function that reads a text file (UTF8) and prints it to stdout
+
+    args: 
+        filename(str) - file name/path to file to be read
+    """
+    with open(filename,"r",encoding="utf-8") as f:
+
+        for line in f:
+            print(line,end="")
+
+
+
+if __name__ == '__main__':
+    read_file("testFile.txt")
